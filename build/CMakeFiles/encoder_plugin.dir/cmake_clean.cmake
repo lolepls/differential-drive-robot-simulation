0ac420a8be9b2b88d02c17d68398f92e3280adf4
Gioele Mombelli <gio_momby@yahoo.it>
2018-07-15 21:53:06 +0200

    Add files via upload

---
+++ build/CMakeFiles/encoder_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/encoder_plugin.dir/encoder_plugin.cc.o"
+  "libencoder_plugin.pdb"
+  "libencoder_plugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/encoder_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
